38b34904ac83292e49f916ebf1d126bdc25db312
Relapnigor <robert.filas@outlook.com>
2026-04-18 19:59:49 +0200

    Merge grammar into one file.

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@ from lark import Lark
 with open("pascal_code.pas", "r") as f:
     pascal_code = f.read()
 
-parser = Lark.open("../grammar/main.lark", parser="lalr", rel_to=__file__)
+parser = Lark.open("../grammar/grammar.lark", parser="lalr", rel_to=__file__)
 
 tree = parser.parse(pascal_code)
 # print(tree.pretty())
